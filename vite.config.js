57fccef0b30df80f0578acec10a27fc5adb2c1f9
Luciano Tadeo Lazarte <126998562+Jehp23@users.noreply.github.com>
2026-01-21 16:14:55 -0300

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/Inferencia-Estadistica-Aplicada-a-las-Finanzas/',
     plugins: [react()],
 })
