97a30a7e88c1b9252dc5ee72889e1ddba8d63561
Luciano Tadeo Lazarte <126998562+Jehp23@users.noreply.github.com>
2026-02-04 16:07:35 -0300

    Configurar despliegue en github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: '/Inferencia-Estadistica-Aplicada-a-las-Finanzas/',
     plugins: [react()],
-    base: "/Inferencia-Estad-stica-Aplicada-a-las-Finanzas/",
 })
